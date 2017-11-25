0e7c17e330743ec44957b9b9a8317de53c9aa42c
Glitch (discord-bot-example) <none>
2017-11-25 11:56:30 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const Eris = require('eris');
+const Eris = require("eris");
  
 const bot = new Eris(process.env.DISCORD_BOT_TOKEN);   // Replace DISCORD_BOT_TOKEN in .env with your bot accounts token
  
-bot.on('ready', () => {                                // When the bot is ready
-    console.log('Ready!');                             // Log "Ready!"
+bot.on("ready", () => {                                // When the bot is ready
+    console.log("Ready!");                             // Log "Ready!"
 });
  
-bot.on('messageCreate', (msg) => {                     // When a message is created
-    if(msg.content.includes('!timebot')) {             // If the message content includes "1337"
-        bot.createMessage(msg.channel.id, 'Now is the winter of our discontent');  // Send a message in the same channel with "damn it"
+bot.on("messageCreate", (msg) => {                     // When a message is created
+    if(msg.content.includes("!timebot")) {             // If the message content includes "1337"
+        bot.createMessage(msg.channel.id,              
+            "Now is the winter of our discontent\n" +
+            "Made glorious summer by this sun of York\n" +
+            "And all the clouds that lour'd upon our house\n" +
+            "In the deep bosom of the ocean buried.");
     }
 });
  
